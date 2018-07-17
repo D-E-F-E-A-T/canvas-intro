97be18f6c21569bea203a7214f13d0e473269d3c
Ravi Kalia <ravkalia@gmail.com>
2018-07-17 16:46:10 -0500

    circles done with arc, using angles - is even easier with x,y co-ordinates

---
+++ circles/main.js
@@ -0,0 +1,13 @@
+const canvas = document.getElementById('example');
+const ctx = canvas.getContext('2d');
+ctx.beginPath();
+const x = 100; // x coordinate
+const y = 75; // y coordinate
+const radius = 50; // Arc radius
+const startAngle = 0; // Starting point on circle
+const endAngle = Math.PI*2; // End point on circle
+ctx.arc(x, y, radius, startAngle, endAngle, true);
+ctx.moveTo(290,75)
+ctx.arc(250, 75, 40, 0, endAngle, true);
+ctx.stroke();
+ctx.fill();
