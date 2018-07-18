aa7b62cc100e4302097f4904d84599d152252e01
Ravi Kalia <ravkalia@gmail.com>
2018-07-18 10:26:12 -0500

    pac man canvas changed to es6

--- circles/main.js
+++ circles/main.js
@@ -7,7 +7,7 @@ const radius = 50; // Arc radius
 const startAngle = 0; // Starting point on circle
 const endAngle = Math.PI*2; // End point on circle
 ctx.arc(x, y, radius, startAngle, endAngle, true);
-ctx.moveTo(290,75)
+ctx.moveTo(290, 75);
 ctx.arc(250, 75, 40, 0, endAngle, true);
 ctx.stroke();
 ctx.fill();
