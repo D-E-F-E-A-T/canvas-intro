a88be211e6114dae760d1d7cd11b9e961c0bd6e9
Ravi Kalia <ravkalia@gmail.com>
2018-07-19 11:19:01 -0500

    two halves of court

--- basketball_court/main.js
+++ basketball_court/main.js
@@ -2,7 +2,9 @@ const draw = () => {
   canvas = document.getElementById('basketball-court');
   ctx = canvas.getContext('2d');
 // outline of exterior
-  ctx.fillRect(x, y, w, h); 
+  ctx.fillRect(0, 0, 400, 600); 
+  ctx.strokeRect(0, 0, 400, 300);  
+  ctx.strokeRect(300, 0, 400, 300);  
 
 };
 
